f4ebffc2833e4c8a4d04119d8e568dd1d1206bf0
KwonSoonYeong <163376481+KwonSoonYeong@users.noreply.github.com>
2025-06-10 13:46:07 +0900

    Add files via upload

---
+++ app/src/main/java/com/am/mytodolistapp/CollaborationActivity.java
@@ -0,0 +1,12 @@
+package com.am.mytodolistapp;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class CollaborationActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_collaboration);  // 레이아웃 연결
+    }
+}
